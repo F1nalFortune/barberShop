32cdc6dc2904ebaabf7f7bfc5893dd67586094f4
Brian <juniortoad1738@gmail.com>
2016-10-17 23:21:56 -0600

    boiler plate

---
+++ client/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return(
+    <div>
+      <footer className="page-footer blue-grey darken-4 footer-div">
+        <div className="footer-copyright">
+          <div className="container">
+          © 2016 Copyright &nbsp; &nbsp; Trade Square
+          <Link className="grey-text text-lighten-4 right space" to="/about">About Us</Link>
+          <Link className="grey-text text-lighten-4 right space" to="/">Home</Link>
+          <Link className="grey-text text-lighten-4 right space" to="https://twitter.com" target="_blank">Twitter</Link>
+          </div>
+        </div>
+      </footer>
+    </div>
+    )
+  }
+}
+
+export default Footer
